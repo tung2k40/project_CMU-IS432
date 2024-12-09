82621783adb34f8ec9ed2f852701a098470a5deb
tungIT <129710302+tung2k40@users.noreply.github.com>
2024-12-09 22:12:33 +0700

    Update main.js
    update

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
       ".content_1, .content_2, .content_3, .content_4"
     );
     window.onload = function() {
-        alert("Chào mừng bạn đến với Ngũ Đại Lão Tin Đà Nẵng")
+        alert("Chào mừng bạn đến với Ngũ Đại Lão Tinh Đà Nẵng")
     };
 
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add("visible"); // Thêm class .visible khi xuất hiện
+            entry.target.classList.add("visible"); 
           } else {
-            entry.target.classList.remove("visible"); // Xóa class .visible khi ra khỏi viewport
+            entry.target.classList.remove("visible"); 
           }
         });
       },
       { threshold: 0.1 }
-    ); // 0.1 nghĩa là phần tử chỉ cần 10% xuất hiện trong viewport là kích hoạt
+    ); 
 
     elements.forEach((element) => {
       observer.observe(element);
